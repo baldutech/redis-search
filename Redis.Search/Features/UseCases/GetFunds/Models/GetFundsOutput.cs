23013de2b178b2b2403089cecbd417d3d822c8c1
baldutech <diego.balduino@xpi.com.br>
2022-02-10 00:28:55 -0300

    feat: criação do caso de uso para consulta de fundo

---
+++ Redis.Search/Features/UseCases/GetFunds/Models/GetFundsOutput.cs
@@ -0,0 +1,10 @@
+﻿namespace Redis.Search.Features.UseCases.GetFunds.Models
+{
+    public class GetFundsOutput
+    {
+        public int? Id { get; set; }
+        public string? Cnpj { get; set; }
+        public string? Name { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}

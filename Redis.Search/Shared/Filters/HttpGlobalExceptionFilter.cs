875bff1397fd0a8c48ba322491a51faa4d1c09bc
baldutech <diego.balduino@xpi.com.br>
2022-02-10 00:29:46 -0300

    feat: criação de tratamento de exceção http

---
+++ Redis.Search/Shared/Filters/HttpGlobalExceptionFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Redis.Search.Shared.Filters
+{
+    public class HttpGlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<HttpGlobalExceptionFilter> _logger;
+
+        public HttpGlobalExceptionFilter(
+            ILogger<HttpGlobalExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger
+                .LogError(context.Exception, "[Middleware][Exception] => {Message}", context.Exception.Message);
+
+            context.ExceptionHandled = true;
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Result = new ObjectResult(new { message = "Something happened... Try again!" });
+        }
+    }
+}

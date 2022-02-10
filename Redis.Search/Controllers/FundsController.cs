6a5044224f44edbdb67a3084e260a288d11eccba
baldutech <diego.balduino@xpi.com.br>
2022-02-10 00:31:32 -0300

    feat: criação da controller de consulta

---
+++ Redis.Search/Controllers/FundsController.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Redis.Search.Features.UseCases.GetFunds.Models;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Redis.Search.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    
+    public class FundsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FundsController(
+            IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Route("funds")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetFundsAsync(
+            [FromQuery] GetFundsInput input,
+            CancellationToken cancellationToken)
+        {
+            if (!input.IsValid())
+            {
+                return BadRequest();
+            }
+
+            var result = await _mediator.Send(input, cancellationToken);
+
+            if (result == null || !result.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(result);
+        }
+    }
+}

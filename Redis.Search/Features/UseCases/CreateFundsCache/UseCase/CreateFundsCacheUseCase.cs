d2350fc3c2cc1e5c833db01d36674cea21e89113
Baldu <diego.balduino@xpi.com.br>
2022-02-18 20:15:02 -0300

    feat: criação do processamento inicial para armazenar o id do fundo

--- Redis.Search/Features/UseCases/CreateFundsCache/UseCase/CreateFundsCacheUseCase.cs
+++ Redis.Search/Features/UseCases/CreateFundsCache/UseCase/CreateFundsCacheUseCase.cs
@@ -1,6 +1,12 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using Redis.Search.Features.UseCases.GetFunds.Models;
+using Redis.Search.Properties;
+using Redis.Search.Shared.Domain.Funds;
 using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,16 +15,37 @@ namespace Redis.Search.Features.UseCases.GetFunds.UseCase
     public class CreateFundsCacheUseCase : IRequestHandler<CreateFundsCacheInput, bool>
     {
         private readonly IDatabase _database;
+        private readonly ILogger<CreateFundsCacheUseCase> _logger;
 
         public CreateFundsCacheUseCase(
-            IDatabase database)
+            IDatabase database,
+            ILogger<CreateFundsCacheUseCase> logger)
         {
+            _logger = logger;
             _database = database;
         }
 
         public async Task<bool> Handle(CreateFundsCacheInput request, CancellationToken cancellationToken)
         {
-            
+            var updatedDate = $"{DateTime.Now:o}";
+            var funds = JsonSerializer.Deserialize<IEnumerable<Fund>>(Resources.funds);
+
+            foreach (var fund in funds ?? new List<Fund>())
+            {
+                try
+                {
+                    await _database.HashSetAsync($"fund:{fund.Id}", new HashEntry[]
+                    {
+                        new HashEntry("updatedDate", updatedDate),
+
+                        new HashEntry("id", fund.Id),
+                    });
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Erro ao persistir cache Redis para o fundo: {fund}", fund.Id);
+                }
+            }
 
             return true;
         }
